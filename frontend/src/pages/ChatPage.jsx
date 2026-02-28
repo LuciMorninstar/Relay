e7e5da4237e3e37b07b737348d0ead690f2c5b3a
Stars_Winner <stars.winner1121@gmail.com>
2026-02-28 23:36:12 +0545

    Implmented feature to show message of both users

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WidthWrapper from '../components/WidthWrapper'
 
 
 import RelaySidebar from '../components/RelaySidebar';
 import ChatWindow from '../components/ChatWindow';
+import { useMessagesById } from '../hooks/useMessage.js';
 
 const ChatPage = () => {
+
+  //This selectedChatFriend is being used in chatContacts component
+  const [selectedChatFriendId, setSelectedChatFriendId] = useState(null);
+  console.log(selectedChatFriendId, "selectedChatFriendId");
+
+  const messageQuery = useMessagesById(selectedChatFriendId);
+  //here storing in a varibale to pass and in chatwindow shall i use messageQuery.data and all for loading and error
+
+  console.log(messageQuery.data, "messageQuery.data"); /*working*/
+
+
+
 
 
 
@@ -15,8 +28,8 @@ const ChatPage = () => {
         <div className = "w-full h-[calc(100vh-100px)]  bg-dark-secondary-color rounded-3xl flex flex-row gap-2 ">
 
 
-            <RelaySidebar/>
-            <ChatWindow/>
+            <RelaySidebar setSelectedChatFriendId={setSelectedChatFriendId}/>
+            <ChatWindow messageQuery = {messageQuery} />
 
             
 
